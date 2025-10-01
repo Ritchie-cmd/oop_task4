70195d6aa2779094c700bf91e5ecdc0e2d42c2de
Ritchie Boy Romagos <ritchiebromagos7@gmail.com>
2025-10-01 14:05:16 +0800

    update

---
+++ lib/delivery_method.dart
@@ -0,0 +1,5 @@
+abstract class DeliveryMethod{
+
+  void deliver(String orderId, String destination);
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'delivery_method.dart';
+import 'order_fulfillment.dart';
+
+void main(){
+
+  print('===== FOOD DELIVERY =====');
+  
+}
