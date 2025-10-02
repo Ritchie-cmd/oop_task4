6184fa814d3c37b303b2a6a25811677cf58a81f2
depende-nimo <elabamiles@gmail.com>
2025-10-02 15:12:47 +0800

    Ikaw bahala main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
-void main(){
+import 'order_fulfillment.dart';
+import 'airplane.dart';
+void main() {
+  
+  var airFulfillment = OrderFulfillment(Airplane());
+  airFulfillment.processOrder("003", "Manila");
+  airFulfillment.shipOrder("004", "Cebu");
+}
+
 
   
-}
